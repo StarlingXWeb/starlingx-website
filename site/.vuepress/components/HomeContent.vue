2ba47d085d764ab91206415d1d89942a0c594ff1
Jimmy McArthur <jimmy@tipit.net>
2019-05-21 11:47:58 -0500

    Adding newsletter to StarlingX
    Signed-off-by: Jimmy McArthur <jimmy@tipit.net>

--- site/.vuepress/components/HomeContent.vue
+++ site/.vuepress/components/HomeContent.vue
@@ -48,6 +48,41 @@
         </div><!-- /.columns -->
       </div><!-- /.section-inner -->
     </base-section>
+
+   <section class="section search-content">
+    <div class="search-content">
+      <h3 class="search-content-title">Subscribe to News About StarlingX</h3>
+      <!-- /.hero-title -->
+      <div class="search-content-subtitle">Stay up to date on StarlingX and Open Infrastructure community news.
+      </div>
+      <div v-if="withEntry" class="hero-entry">
+        <slot name="entry"/>
+      </div>
+      <!-- /.hero-entry -->
+      <div v-if="withButton" class="hero-actions">
+        <slot name="buttons"/>
+      </div>
+      <!-- /.hero-actions -->
+    </div>
+    <b-field position="is-centered">
+      <form method="post" id="e2ma_signup" onsubmit="return signupFormObj.checkForm(this)" action="https://app.e2ma.net/app2/audience/signup/1900237/1771360/">
+        <input id="id_prev_member_email" name="prev_member_email" type="hidden">
+        <input id="id_source" name="source" type="hidden">
+        <input id="id_group_4036448" name="group_4036448" type="hidden" value="4036448">
+        <input type="hidden" name="private_set" value="{num_private}">
+        <input placeholder="Email Address" id="id_email" name="email" type="email" required class="search-container-input" size="is-large">
+        </input>
+          <!-- <button class="search-container-submit">Search</button> -->
+          <a href="#" class="button is-primary no-border-search-submit" onclick="document.getElementById('e2ma_signup').submit();">
+            <span>SUBMIT</span>
+            
+            <span class="ico">
+              <img :src="require('../theme/svg/arrow-left.svg')" alt="Call To Action">
+            </span>
+          </a>
+      </form>
+    </b-field>
+    </section>
   </main><!-- /.main -->
 </template>
 
